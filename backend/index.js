14a714a1d2b05f561045590d01185406f6d20c4d
Sorin <neagusorinel03@gmail.com>
2024-06-19 19:59:56 +0300

    final backend

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,12 @@ import reviewRoutes from "./routes/review.js";
 import userRoutes from "./routes/user.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
-
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
